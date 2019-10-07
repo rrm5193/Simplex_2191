13dde4de88a4c5dce0cdd9612a54f55bab0c17e1
Ryan Montero <rrm5193@ad.rit.edu>
2019-10-07 14:36:36 -0400

    Got the base positions for all camera scenes, still need to mess with the fov and planes

--- E08 - Projections/AppClass.cpp
+++ E08 - Projections/AppClass.cpp
@@ -54,21 +54,37 @@ void Application::Display(void)
 		break;
 	case 2:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPerspective(false);
+		m_pCamera->SetHorizontalPlanes(vector2(-5.0f,5.0f));
+		m_pCamera->SetVerticalPlanes(vector2(-5.0f, 5.0f));
 		break;
 	case 3:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPerspective(false);
+		m_pCamera->SetPositionTargetAndUpward(vector3(10.0f , 0.0f, 0.0f) , vector3(0.0f, 0.0f, 0.0f), vector3(0.0f, 0.0f, -1.0f));
+		m_pCamera->SetHorizontalPlanes(vector2(-20.0f, 20.0f));
+		m_pCamera->SetVerticalPlanes(vector2(-20.0f, 20.0f));
 		break;
 	case 4:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPerspective(false);
+		m_pCamera->SetPositionTargetAndUpward(vector3(0.0f, 0.0f, -20.0f), vector3(0.0f, 0.0f, 0.0f), vector3(0.0f, 1.0f, 0.0f));
+
 		break;
 	case 5:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPerspective(false);
+		m_pCamera->SetPositionTargetAndUpward(vector3(0.0f, 0.0f, -10.0f), vector3(0.0f, 0.0f, 0.0f), vector3(0.0f, 1.0f, 0.0f));
+		
 		break;
 	case 6:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPerspective(false);
+		m_pCamera->SetPositionTargetAndUpward(vector3(0.0f, 0.0f, 0.0f), vector3(0.0f, 0.0f, -5.0f), vector3(0.0f, 1.0f, 0.0f));
 		break;
 	case 7:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(vector3(0.0f,0.0f,10.0f), vector3(0.0f,0.0f,0.0f), vector3(0.0f, -1.0f, 0.0f));
 		break;
 	}
 
